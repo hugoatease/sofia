773171502938dfe7dcfa3ca47a73780ffa97571a
Hugo Caille <hugo@caille.me>
2020-09-12 18:34:01 +0200

    feat: add hourly forecast to ClockWeather

--- packages/receiver/src/components/ClockWeather.js
+++ packages/receiver/src/components/ClockWeather.js
@@ -1,6 +1,6 @@
 import React, { useState, useCallback } from "react";
 import styled from "styled-components";
-import { rhythm, scale } from "ui";
+import { rhythm, scale, Widget } from "ui";
 import { useHarmonicIntervalFn } from "react-use";
 import axios from "axios";
 import useSwr from "swr";
@@ -9,25 +9,40 @@ import { fr } from "date-fns/locale";
 import { useConfig } from "hooks";
 import "weather-icons-sass/css/weather-icons.css";
 
-const ClockWeatherWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  background-color: rgba(0, 0, 0, 0.6);
-  color: #fff;
-  padding: ${rhythm(0.5)};
-  ${scale(1)};
+const ClockWeatherWrapper = styled(Widget)`
+  font-family: "Roboto Mono";
+`;
 
+const Clock = styled.div``;
+
+const CurrentWeather = styled.div`
+  ${scale(1)};
   i {
     margin-right: ${rhythm(1 / 4)};
   }
 `;
 
-const Clock = styled.div`
-  font-family: "Roboto Mono";
+const Line = styled.div`
+  display: flex;
+  justify-content: space-between;
 `;
 
-const CurrentWeather = styled.div`
-  ${scale(0.5)};
+const HourlyForecast = styled(Widget)`
+  font-family: "Roboto Mono";
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ForecastItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 ${rhythm(1 / 4)};
+  ${scale(0.25)};
+
+  i {
+    ${scale(1)};
+  }
 `;
 
 const ClockWeather = () => {
@@ -59,19 +74,45 @@ const ClockWeather = () => {
     setTime(new Date());
   }, 1000);
 
+  const getIconName = ({ dt, weather }) => {
+    const { sunset, sunrise } = weatherData?.current || {};
+
+    if (dt > sunset && dt < sunrise + 24 * 3600) {
+      return `wi wi-owm-night-${weather?.[0]?.id}`;
+    }
+    return `wi wi-owm-day-${weather?.[0]?.id}`;
+  };
+
   return (
-    <ClockWeatherWrapper>
-      <div>
-        <div style={scale(0.5)}>
-          {format(time, "cccc d MMMM", { locale: fr })}
-        </div>
-        <Clock>{format(time, "HH:mm")}</Clock>
-      </div>
-      <CurrentWeather>
-        <i className={`wi wi-owm-${weatherData?.current?.weather?.[0]?.id}`} />
-        {Math.floor(weatherData?.current?.temp)} °C
-      </CurrentWeather>
-    </ClockWeatherWrapper>
+    <>
+      <ClockWeatherWrapper>
+        <Line>
+          <Clock>{format(time, "HH:mm")}</Clock>
+          <CurrentWeather>
+            <i
+              className={getIconName({
+                dt: weatherData?.current?.dt,
+                weather: weatherData?.current?.weather,
+              })}
+            />
+            {Math.floor(weatherData?.current?.temp)}°C
+          </CurrentWeather>
+        </Line>
+        <Line>
+          <div style={scale(0.5)}>
+            {format(time, "cccc d MMMM", { locale: fr })}
+          </div>
+        </Line>
+      </ClockWeatherWrapper>
+      <HourlyForecast>
+        {(weatherData?.hourly || []).slice(1, 13).map(({ dt, weather }) => (
+          <ForecastItem>
+            <i className={getIconName({ dt, weather })} />
+            {format(dt * 1000, "H")}h
+          </ForecastItem>
+        ))}
+      </HourlyForecast>
+    </>
   );
 };
 
--- packages/receiver/src/ui/index.js
+++ packages/receiver/src/ui/index.js
@@ -1,6 +1,14 @@
 import styled from "styled-components";
 import Typography from "typography";
 
+export const typography = new Typography({
+  baseFontSize: "24px",
+  baseLineHeight: 2,
+});
+
+export const scale = typography.scale;
+export const rhythm = typography.rhythm;
+
 export const Container = styled.div`
   display: grid;
   width: 100vw;
@@ -12,12 +20,14 @@ export const Container = styled.div`
 export const Widgets = styled.div`
   display: flex;
   flex-direction: column;
+  > * + * {
+    margin-top: ${rhythm(1 / 2)};
+  }
 `;
 
-export const typography = new Typography({
-  baseFontSize: "26px",
-  baseLineHeight: 3,
-});
-
-export const scale = typography.scale;
-export const rhythm = typography.rhythm;
+export const Widget = styled.div`
+  background-color: rgba(0, 0, 0, 0.6);
+  color: #fff;
+  padding: ${rhythm(0.5)};
+  ${scale(1)};
+`;
